72d572e41943c7acd9969c7fb0a24e17bea8a454
ArthurOliCode <arthur.rilber@gmail.com>
2026-04-04 20:55:04 -0300

    Modelando o projeto para o modelo MVC

---
+++ arquiteturaspring/arquiteturaspring/src/main/java/com/example/arquiteturaspring/todos/TodoRepository.java
@@ -0,0 +1,8 @@
+package com.example.arquiteturaspring.todos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// @Repository --> É opcional, pois esse é um dos únicos casos em que a classe é escaneada automaticamente devido ao JPA
+public interface TodoRepository extends JpaRepository<TodoEntity, Integer> {
+}
---
+++ arquiteturaspring/arquiteturaspring/src/main/java/com/example/arquiteturaspring/todos/TodoService.java
@@ -0,0 +1,23 @@
+package com.example.arquiteturaspring.todos;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    // @AutoWired não se faz necessário, pois, automaticamente o Spring entende a injeção, por ser um Bean gerenciado.
+    private TodoRepository repository; // Foi utilizado o private apenas para encapsulamento.
+
+    public TodoService(TodoRepository todoRepository){ // Construtor se faz necessário para a injeção de dependências do Repository para o Service
+        this.repository = todoRepository;
+    }
+    /*
+        Caso haja mais de um repository para a injeção de dependências, se faz
+        necessário apenas adicionar a vírgula após o que já foi declarado.
+        ex: public TodoService(TodoRepository todorepository, OutroRepository, outroRepository)
+    */
+
+    public TodoEntity salvar(TodoEntity novoTodo){
+        return repository.save(novoTodo);
+    }
+}
