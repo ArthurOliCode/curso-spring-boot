adc410399fd38871c62ca6345a11281034c2e3d3
ArthurOliCode <arthur.rilber@gmail.com>
2026-04-04 20:33:04 -0300

    Utilizando JPA na criação de um to do Entity

--- arquiteturaspring/arquiteturaspring/src/main/java/com/example/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ arquiteturaspring/arquiteturaspring/src/main/java/com/example/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -17,7 +17,7 @@ public class TesteFabricaController {
 
     @Autowired // Auto-gestão do Spring com container e dependências no @Bean.
     // @Qualifier("motorEletrico") --> Tem a função de identificar para o @Autowired qual @Bean de mesmo tipo será utilizado.
-    @Turbo // Annotation personalizada para definir o tipo de motor, fazendo uma injeção de dependência. 
+    @Turbo // Annotation personalizada para definir o tipo de motor, fazendo uma injeção de dependência.
     private Motor motor;
 
     @PostMapping
